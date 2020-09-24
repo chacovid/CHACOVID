b8f32b3a4eb4e1809aa621291f2d462a8a00b72d
Santiago Risso <santiago.risso2@gmail.com>
2020-09-23 22:01:25 -0300

    Modelos y Base de datos v0.8

---
+++ Aplicacion/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from Aplicacion.models import *
+
+# Register your models here.
--- Aplicacion/views.py
+++ Aplicacion/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
+from Aplicacion.models import *
 
 def home(request):
     return render(request, "home.html")
@@ -9,10 +10,13 @@ def permisos(request):
     return render(request, "permisos.html")
 
 def cuidados(request):
-     return render(request, 'cuidados.html')
+    return render(request, 'cuidados.html')
 
 def fases(request):
-     return render(request, 'fases.html')
+    return render(request, 'fases.html')
 
 def mapa(request):
-     return render(request, 'mapa.html')
+    datos = datos.objects.all()
+    context  = {"object_list": datos }
+    return render(request, 'mapa.html', context)
+
